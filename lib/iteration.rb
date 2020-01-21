7f8e8658f3fef2b6abb65fbca35f73e6f6d2009b
Zach Brown <zachmb22@gmail.com>
2020-01-21 01:28:04 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -58,7 +58,7 @@ while row_index < src.count do
   element_index = 0
   while element_index + 1 < src[row_index].count do
     if src[row_index][element_index] + src[row_index][element_index + 1] % 2 == 0
-      sum = sum + src[row_index][element_index] + src[row_index][element_index + 1]
+      sum += src[row_index][element_index] + src[row_index][element_index + 1])
     end
     element_index += 1
   end
